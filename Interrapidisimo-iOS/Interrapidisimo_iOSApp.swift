3a2aac6b257ee729d315e3cdb3d4c900255c51b6
Jorge Luis Rivera Ladino <riveraladinojorgeluis@gmail.com>
2026-02-25 12:39:55 -0500

    Initial Commit

---
+++ Interrapidisimo-iOS/Interrapidisimo_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interrapidisimo_iOSApp.swift
+//  Interrapidisimo-iOS
+//
+//  Created by Jorge Luis Rivera on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Interrapidisimo_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
